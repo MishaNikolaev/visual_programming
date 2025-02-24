3b9f5432090163ca8c2fb968ea257923b438589f
MishaNikolaev <nmichail154@mail.ru>
2025-02-25 00:23:52 +0700

    dz02

---
+++ dz02/orderBy.js
@@ -0,0 +1,69 @@
+function QuickSortV2(array, L, R) {
+    if (L < R) {
+        let i = L, j = R;
+        let pivot = array[Math.floor((L + R) / 2)];
+
+        while (i <= j) {
+            while (compare(array[i], pivot) < 0) {
+                i++;
+            }
+            while (compare(array[j], pivot) > 0) {
+                j--;
+            }
+
+            if (i <= j) {
+                [array[i], array[j]] = [array[j], array[i]];
+                i++;
+                j--;
+            }
+        }
+
+        if (L < j) {
+            QuickSortV2(array, L, j);
+        }
+        if (i < R) {
+            QuickSortV2(array, i, R);
+        }
+    }
+}
+
+function compare(a, b) {
+    if (typeof a !== 'object' || typeof b !== 'object' || !a.name || !a.age || !b.name || !b.age) {
+        throw new Error("Both elements should be objects with 'name' and 'age' properties.");
+    }
+
+    if (a.age < b.age) return -1;   
+    if (a.age > b.age) return 1;
+    if (a.name < b.name) return -1; 
+    if (a.name > b.name) return 1;
+
+    return 0;
+}
+
+function orderBy(array) {
+    if (!array.every(item => typeof item === 'object' && item !== null)) {
+        throw new Error('Elements aint object epta');
+    }
+
+    if (!array.every(item => 'name' in item && 'age' in item)) {
+        throw new Error('gde peresilki, gde sravnenia, chto eto za hernya');
+    }
+
+    const sortedArray = [...array];
+    QuickSortV2(sortedArray, 0, sortedArray.length - 1);
+    return sortedArray;
+}
+
+
+const data = [
+     { name: 'Anton', age: 23 },
+    { name: 'Misha', age: 19 },
+    { name: 'Katya', age: 19 },
+    { name: 'Kuku Epta', age: 100 },
+    { name: 'Dima', age: 20 },
+    { name: 'Dima', age: 19 }
+];
+
+console.log(orderBy(data)); 
+
+module.exports = orderBy;
---
+++ dz02/test/test.js
@@ -0,0 +1,45 @@
+const orderBy = require('../orderBy');
+
+describe('orderBy function', () => {
+    test('correct sort by name and age', () => {
+        const data = [
+            { name: 'Anton', age: 23 },
+            { name: 'Misha', age: 19 },
+            { name: 'Katya', age: 19 },
+            { name: 'Kuku Epta', age: 100 },
+            { name: 'Dima', age: 20 },
+            { name: 'Dima', age: 19 }
+        ];
+
+        const sortedData = orderBy(data);
+
+        expect(sortedData).toEqual([
+            { name: 'Dima', age: 19 },
+            { name: 'Katya', age: 19 },
+            { name: 'Misha', age: 19 },
+            { name: 'Dima', age: 20 },
+            { name: 'Anton', age: 23 },
+            { name: 'Kuku Epta', age: 100 }
+        ]);
+    });
+
+    test('should throw error because array contains non object', () => {
+        const data = [
+            { name: 'Anton', age: 23 },
+            'Misha', 
+            { name: 'Katya', age: 19 }
+        ];
+
+        expect(() => orderBy(data)).toThrow('Elements aint object epta');
+    });
+
+    test('should throw error cause field age is absent', () => {
+        const data = [
+            { name: 'Anton', age: 23 },
+            { name: 'Misha' },
+            { name: 'Katya', age: 19 }
+        ];
+
+        expect(() => orderBy(data)).toThrow('gde peresilki, gde sravnenia, chto eto za hernya');
+    });
+});
